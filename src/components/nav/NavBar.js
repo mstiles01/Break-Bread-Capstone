995693306993624cedece59440eac1aaadd196d4
Michael Stiles <mstiles01@icloud.com>
2019-09-10 15:15:35 -0500

    register/login render

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -8,15 +8,18 @@ class NavBar extends Component {
     }
 
     render() {
-        return (<nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
-        <ul className="nav nav-pills nav-fill">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-          </li>)
-          </ul>
-          </nav>
-    );
-    }
-}
+        return (
+            <nav className="navbar navbar-light light-blue flex-md-nowrap p-0 shadow">
+              <ul className="nav nav-pills nav-fill">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          );
+        }
+      }
+
+      export default NavBar;
